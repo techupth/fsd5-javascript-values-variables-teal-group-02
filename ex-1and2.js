e48db2b8ed0b030e5f7f34f6705d509116977c3f
Dennis <suwatchai.kaew@gmail.com>
2023-11-19 10:50:59 +0700

    feat: add exercise V1 (dennis)

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,14 +1,20 @@
 //Exercise #1: Variable Creation
 //Start coding here
-let fullName = "Karter";
+// 1.1
+let firstName = "Karter";
+// 1.2
 let lastName = "Anderson";
+// 1.3
 let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
-
+// 1.4
 const restaurantName = "Chef's Table by Chef James";
+// 1.5
 const netPrice = 15000;
+// 1.6
 const numberOfOpenRestaurant = 1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
 address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
-netPrice = 2500;
+netPrice = 5000;
+numberOfOpenRestaurant = 2500;
