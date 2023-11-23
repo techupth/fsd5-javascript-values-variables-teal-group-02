a04d8f4db75d51461ee87a9cd88e97969478ba4f
Dennis <suwatchai.kaew@gmail.com>
2023-11-23 16:16:38 +0700

    fix update

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,16 +1,16 @@
 //Exercise #1: Variable Creation
 let name = "Karter";
 let surname = "Anderson";
-let adress = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
-const shopname = "Chef's Table by Chef James";
-const billprice = 15000;
-const totalenabledshop = 1500;
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+const shopName = "Chef's Table by Chef James";
+const billPrice = 15000;
+const totalEnabledShop = 1500;
 //Exercise #2: Variable Reassignment
-adress = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
-console.log(adress);
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+console.log(address);
 
-billprice = 5000;
-console.log(billprice);
+billPrice = 5000;
+console.log(billPrice);
 
-totalenabledshop = 2500;
-console.log(totalenabledshop);
+totalEnabledShop = 2500;
+console.log(totalEnabledShop);
